6f3312fe6d0f97b105163d31043f1cbe90b77814
kvyaceslav <kvyaceslav@gmail.com>
2023-03-27 17:18:28 +0300

    Spread urls

--- project/urls.py
+++ project/urls.py
@@ -1,20 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from products.views import (
-    index as product_index,
-    show as product_show,
-    add as product_add,
-    update as product_update,
-    delete as product_delete,
-)
+from django.urls import path, include
 from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', views.obtain_auth_token),
-    path('api/product/', product_index),
-    path('api/product/<int:id>', product_show),
-    path('api/product/add', product_add),
-    path('api/product/update/<int:id>', product_update),
-    path('api/product/delete/<int:id>', product_delete),
+    path('api/product/', include('products.urls')),
 ]
